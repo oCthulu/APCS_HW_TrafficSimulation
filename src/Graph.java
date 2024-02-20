d542528f0006d5c34d9eced97768ac60fdc42428
oCthulu <107318894+oCthulu@users.noreply.github.com>
2024-02-20 11:14:10 -0500

    Added framework for graphs

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,8 @@
-public class Graph<NodeT extends Node> {
+import java.util.LinkedList;
+import java.util.List;
 
+public class Graph extends GenericGraph<Graph.Node> {
+    //empty data holder node class
+    //in other implementations of GenericGraph this could be made to hold extra data
+    public class Node extends GenericGraph<Graph.Node>.GenericNode{}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Graph graph = new Graph();
+        Graph.Node a = graph.new Node();
     }
 }
