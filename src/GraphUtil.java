ebaed93f3d94d113f93f021a4dae111a5b03a013
kieranchalk <kchalk2008@gmail.com>
2024-02-23 08:39:35 -0500

    transverse simulation

--- src/GraphUtil.java
+++ src/GraphUtil.java
@@ -1,9 +1,27 @@
+import java.util.Random;
 public class GraphUtil {
     private GraphUtil() throws Exception{
         throw new Exception("Cannot create instance of GraphUtil class.");
     }
 
     static record SquareGraphReturnResult(Graph graph, Graph.Node startingNode, Graph.Node endNode){
+    }
+
+    public static int runTraverseSimulation(Graph currentGraph, Graph.Node startNode, Graph.Node endNode, Random nodeChoiceRandom){
+        Graph.Node currentNode = startNode;
+        int maximumAmountOfTurns = 100;
+
+        for (int i = 0; i< maximumAmountOfTurns; i++){
+            if (currentNode.getConnectedNodes().isEmpty()){
+                return maximumAmountOfTurns;
+            }
+            if (currentNode == endNode){
+                return i;
+            }
+            int randomNode = nodeChoiceRandom.nextInt(currentNode.getConnectedNodes().size());
+            currentNode = currentNode.getConnectedNodes().get(randomNode);
+        }
+        return maximumAmountOfTurns;
     }
 
 
